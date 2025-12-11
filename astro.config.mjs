04cd25577b8ba7ab8baf476a58b632470c691835
erikofantastiko <134730849+erikofantastiko@users.noreply.github.com>
2025-12-11 21:09:46 +0100

    updated astro.config.mjs to include custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from 'astro/config'
 
 export default defineConfig({
-  site: 'https://IronForgeFitness.github.io',
-  base: '/IronForgeLanding',
+  site: 'hhttps://ironforge.app/',
 })
